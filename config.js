873a2447dba755e8ee3e058f4215b9e6cfe8750b
mrnaduwav4 <vnaduwa@gmail.com>
2024-08-10 23:44:57 +0530

    MR.NADUWA-V1

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/04f33ed3a84d24104a9ef.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "HELLOW I AM MR.NADUWA ,I AM ALIVE NOW",
 };
