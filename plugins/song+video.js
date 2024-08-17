e0aadd0c1725790afba83b82e61c5e437dc78fdf
𝗛.𝗔.𝗡𝗔𝗗𝗨𝗡 𝗦𝗔𝗗𝗜𝗦𝗛𝗞𝗔 <vnaduwa@gmail.com>
2024-08-17 21:03:31 +0530

    Update song+video.js

--- plugins/song+video.js
+++ plugins/song+video.js
@@ -35,8 +35,58 @@ let down = await fg.yta(url)
 let downloadUrl = down.dl_url
 
 
-//send audio message
+//send audio+ document  message
 await conn.sendMessage(from,{audio: {url:downloadUrl},mimetype:"audio/mpeg"},{quoted:mek})
+await conn.sendMessage(from,{document : {url:downloadUrl},mimetype:"audio/mpeg",filName:data.title + ".mp3",caption:"✅MADE BY MR.NADUWA-V1✅"},{quoted:mek})
+
+  
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
+
+
+
+//=============video-dl===============
+
+cmd({
+    pattern: "video",
+    desc: "download videos",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply("please give me url or title")
+const search = await yts(q)
+const data = search.videos[0];
+const url = data.url
+
+let desc = `
+✅MR.NADUWA-V1 VIDEO DOWNLOADER✅
+
+
+title: ${data.title}
+description: ${data.description}
+time: ${data.timestamp}
+ago: ${data.ago}
+views: ${data.views}
+
+
+✅MADE BY MR.NADUWA-V1✅
+`
+
+await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});
+//download video
+
+let down = await fg.ytv(url)
+let downloadUrl = down.dl_url
+
+
+//send video+ document message
+await conn.sendMessage(from,{video: {url:downloadUrl},mimetype:"video/mp4"},{quoted:mek})
+await conn.sendMessage(from,{document : {url:downloadUrl},mimetype:"video/mp4",filName:data.title + ".mp4",caption:"✅MADE BY MR.NADUWA-V1"},{quoted:mek})
 
   
 }catch(e){
