d1320d4cbf950bac8ecbe81f6a3e4a7ad7e7572e
𝗛.𝗔.𝗡𝗔𝗗𝗨𝗡 𝗦𝗔𝗗𝗜𝗦𝗛𝗞𝗔 <vnaduwa@gmail.com>
2024-08-17 22:45:11 +0530

    MR.NADUWA-V1

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
-ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/04f33ed3a84d24104a9ef.jpg",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/c8a560f198364fbe93261.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "HELLOW I AM MR.NADUWA ,I AM ALIVE NOW",
 };
