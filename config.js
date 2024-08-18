1fca3ea175d8a27b08adaa1157ff884632adeed2
𝗛.𝗔.𝗡𝗔𝗗𝗨𝗡 𝗦𝗔𝗗𝗜𝗦𝗛𝗞𝗔 <vnaduwa@gmail.com>
2024-08-19 00:29:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,26 +7,6 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/6fea4e5e00bc0d9395f15.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "
-    ```PREFIX    : .
-MENU      : not-yet
-VERSION   : 1.0.0
-PLUGINS   : 5
-E-PLUGINS :not-yet
-SUDO      : 
-AUTO READ MSG    : ❎-not-yet
-AUTO STATUS VIEW : ✅
-AUTO REJECT CALL : ❎-not-yet
-ALWAYS ONLINE    : ✅
-ANTI DELETE MSG  : ❎-not-yet
-
-
-Channel : no-yet
-
-Plugins : no-yet
-
-FAQ : no-yet```
-
-*ReadMore :* - not-yet",
+ALIVE_MSG: process.env.ALIVE_MSG || "🤖𝗛𝗘𝗬 𝗜 𝗔𝗠 𝗠𝗥.𝗡𝗔𝗗𝗨𝗪𝗔-𝗩1 𝗪𝗛𝗔𝗧𝗦𝗔𝗣𝗣 𝗕𝗢𝗧 🤖𝙳𝙴𝙿𝙻𝚈 𝙱𝚈 𝙼𝚁.𝙽𝙰𝙳𝚄𝚆𝙰☀",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 };
