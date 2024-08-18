cabd127c2494de62cf430ecec3978a10e42e9207
𝗛.𝗔.𝗡𝗔𝗗𝗨𝗡 𝗦𝗔𝗗𝗜𝗦𝗛𝗞𝗔 <vnaduwa@gmail.com>
2024-08-18 14:28:44 +0530

    MR.NADUWA-V1

--- config.js
+++ config.js
@@ -8,4 +8,5 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/c8a560f198364fbe93261.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "HELLOW I AM MR.NADUWA ,I AM ALIVE NOW",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 };
