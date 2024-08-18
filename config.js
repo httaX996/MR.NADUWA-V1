d10676815a6050223380f60bdd4f9c3c45cfaeca
𝗛.𝗔.𝗡𝗔𝗗𝗨𝗡 𝗦𝗔𝗗𝗜𝗦𝗛𝗞𝗔 <vnaduwa@gmail.com>
2024-08-19 00:20:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,27 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
-ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/c8a560f198364fbe93261.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "HELLOW I AM MR.NADUWA ,I AM ALIVE NOW",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/6fea4e5e00bc0d9395f15.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "
+    ```PREFIX    : .
+MENU      : not-yet
+VERSION   : 1.0.0
+PLUGINS   : 5
+E-PLUGINS :not-yet
+SUDO      : 
+AUTO READ MSG    : ❎-not-yet
+AUTO STATUS VIEW : ✅
+AUTO REJECT CALL : ❎-not-yet
+ALWAYS ONLINE    : ✅
+ANTI DELETE MSG  : ❎-not-yet
+
+
+Channel : no-yet
+
+Plugins : no-yet
+
+FAQ : no-yet```
+
+*ReadMore :* - not-yet",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 };
