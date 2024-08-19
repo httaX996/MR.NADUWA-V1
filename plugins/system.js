3c4a7b2e8daa5a0d18c100544d00fcc02da30fa0
𝗛.𝗔.𝗡𝗔𝗗𝗨𝗡 𝗦𝗔𝗗𝗜𝗦𝗛𝗞𝗔 <vnaduwa@gmail.com>
2024-08-19 18:46:56 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -15,7 +15,7 @@ try{
 let status = `*Uptime:*  ${runtime(process.uptime())}
 *Ram usage:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
 *HostName:* ${os.hostname()}
-*Owner:* MR NADUWA-V1✅DEPLY BY MR-NADUWA
+*Owner:* MR NADUWA-V1-DEPLY BY MR-NADUWA
 `
 
 return reply(`${status}`)
