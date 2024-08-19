54962eb5c43e33a7aff2f62f899c080e0e54a2b5
𝗛.𝗔.𝗡𝗔𝗗𝗨𝗡 𝗦𝗔𝗗𝗜𝗦𝗛𝗞𝗔 <vnaduwa@gmail.com>
2024-08-19 18:22:27 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -12,10 +12,10 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-let status = `📌*Uptime:*  ${runtime(process.uptime())}
-📌*Ram usage:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
-📌*HostName:* ${os.hostname()}
-📌*Owner:* MR.NADUWA-V1✅DEPLY BY MR-NADUWA
+let status = `*Uptime:*  ${runtime(process.uptime())}
+*Ram usage:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+*HostName:* ${os.hostname()}
+*Owner:* MR.NADUWA-V1✅DEPLY BY MR-NADUWA
 `
 
 return rereply(`${status}`)
@@ -23,5 +23,7 @@ return rereply(`${status}`)
 }catch(e){
   console.log(e)
   reply(`${e}`)
+
+    
 }
 })
