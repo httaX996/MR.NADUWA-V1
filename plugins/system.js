c94390df3f1f071afe02db9e9837175caa280daa
𝗛.𝗔.𝗡𝗔𝗗𝗨𝗡 𝗦𝗔𝗗𝗜𝗦𝗛𝗞𝗔 <vnaduwa@gmail.com>
2024-08-19 18:09:47 +0530

    MR.NADUWA-V1

--- plugins/system.js
+++ plugins/system.js
@@ -1,6 +1,7 @@
 const config = require('../config')
 const {cmd , commands} = require('../command')
 const os = required("os")
+const {runtime} = require(`../lib/functions`)
 
 cmd({
     pattern: "system",
