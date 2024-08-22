7b50c4643a7891b259d835f02c208c4f4484aa2d
𝗛.𝗔.𝗡𝗔𝗗𝗨𝗡 𝗦𝗔𝗗𝗜𝗦𝗛𝗞𝗔 <vnaduwa@gmail.com>
2024-08-22 15:05:15 +0530

    MR.NADUWA-V1

---
+++ plugins/menu.js
@@ -0,0 +1,64 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+
+cmd({
+    pattern: "panel",
+    desc: "get cmd list",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let menu = {
+main: '',
+download: '',
+group: '',
+owner: '',
+convert: '',
+search: ''
+};
+
+for (let i = 0; i < commands.length; i++) {
+if (commands[i].pattern && !commands[i].dontAddCommandList) {
+menu[commands[i].category] += `.${commands[i].pattern}\n`;
+ }
+}
+
+let madeMenu = `👋 *Hello ${pushname}*
+> ⚖️ *DOWNLOAD COMMANDS* ⚖️
+
+${menu.download}
+
+> ⚖️ *MAIN COMMANDS* ⚖️
+
+${menu.main}
+
+> ⚖️ *GROUP COMMANDS* ⚖️
+
+${menu.group}
+
+> ⚖️ *OWNER COMMANDS* ⚖️
+
+${menu.owner}
+
+> ⚖️ *CONVERT COMMANDS* ⚖️
+
+${menu.convert}
+
+> ⚖️ *SEARCH COMMANDS* ⚖️
+
+${menu.search}
+
+✅ *_POWERD BY MR.NADUWA-V1_* ✅
+
+      
+    
+    ` 
+await conn.sendMessage(from,{text:madeMenu,{quoted:mek})
+  
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+  
