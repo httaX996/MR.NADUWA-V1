451e2b21fae8fd1962105586aa349ceb4a69fbfd
𝗛.𝗔.𝗡𝗔𝗗𝗨𝗡 𝗦𝗔𝗗𝗜𝗦𝗛𝗞𝗔 <vnaduwa@gmail.com>
2024-09-11 23:27:46 +0530

    MR.NADUWA-V1

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "tBhQFJaa#fFQoi7gGFc4ziI6nSSB-ObQXEzhEEFGb6eNIvX-Qbzs",
-MONGODB: process.env.MONGODB || "mongodb://mongo:HAODxgQcEsjjUWFAdOwxhJQYXuIBCWMU@junction.proxy.rlwy.net:40219",
+SESSION_ID: process.env.SESSION_ID || "0QxEjLhJ#tX5JG5O3wsjYpX6sXFr09pYpNKGPWQuEoDp3WImN4Ds",
+MONGODB: process.env.MONGODB || "mongodb://mongo:mvUcSsatPnZFIrdAhxAkDDrAFKpTNKeb@junction.proxy.rlwy.net:53702",
 };
