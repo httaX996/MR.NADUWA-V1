739be732bf793499880953d16889c6960be90b37
𝗛.𝗔.𝗡𝗔𝗗𝗨𝗡 𝗦𝗔𝗗𝗜𝗦𝗛𝗞𝗔 <vnaduwa@gmail.com>
2024-11-29 20:05:08 +0530

    MR.NADUWA-V1✅

--- config.js
+++ config.js
@@ -10,6 +10,7 @@ ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/6fea4e5e00bc0d9395f
 ALIVE_MSG: process.env.ALIVE_MSG || "🤖𝗛𝗘𝗬 𝗜 𝗔𝗠 𝗠𝗥.𝗡𝗔𝗗𝗨𝗪𝗔-𝗩1 𝗪𝗛𝗔𝗧𝗦𝗔𝗣𝗣 𝗕𝗢𝗧 🤖𝙳𝙴𝙿𝙻𝚈 𝙱𝚈 𝙼𝚁.𝙽𝙰𝙳𝚄𝚆𝙰☀",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "private",
-ALWAYS_ONLINE : process.env.ALWAYS_ONLINE || "false",    
-    
+ALWAYS_ONLINE : process.env.ALWAYS_ONLINE || "false",
+AUTO_TYPING: process.env.AUTO_TYPING || "false",
+ AUTO_RECORDING: process.env.AUTO_RECORDING || "true",   
 };
