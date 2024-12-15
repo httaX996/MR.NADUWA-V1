6cbd4848a81c7d068b9c1eb0d5c598214dfa7928
𝗛.𝗔.𝗡𝗔𝗗𝗨𝗡 𝗦𝗔𝗗𝗜𝗦𝗛𝗞𝗔 <vnaduwa@gmail.com>
2024-12-15 22:41:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -13,5 +13,6 @@ MODE: process.env.MODE || "private",
 ALWAYS_ONLINE : process.env.ALWAYS_ONLINE || "false",
 AUTO_TYPING: process.env.AUTO_TYPING || "false",
  AUTO_RECORDING: process.env.AUTO_RECORDING || "false", 
+ MONGODB: process.env.MONGODB || "enter mongodb here",   
 
 };
