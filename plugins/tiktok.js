aaf1aea7bbb7e8828099e6d5eb6d5fc8a8f80ed0
𝗛.𝗔.𝗡𝗔𝗗𝗨𝗡 𝗦𝗔𝗗𝗜𝗦𝗛𝗞𝗔 <vnaduwa@gmail.com>
2024-12-27 22:48:03 +0530

    Create tiktok.js

---
+++ plugins/tiktok.js
@@ -0,0 +1,58 @@
+// TIKTOK DOWNLOAD COMMAND
+
+const { cmd } = require('../command')
+const { fetchJson } = require('../lib/functions')
+
+const apilink = 'https://www.dark-yasiya-api.site' // API LINK ( DO NOT CHANGE THIS!! )
+
+
+cmd({
+    pattern: "tiktok",
+    alias: ["tt","ttdown"],
+    react: "",
+    desc: "",
+    category: "download",
+    use: '.tiktok < url >',
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, reply, q }) => {
+try{
+  
+if(!q) return await reply("Please give me tiktok url");
+  if(!q.includes('tiktok.com')) return await reply("This url is invalid");
+  
+const tiktok = await fetchJson(`${apilink}/download/tiktok?url=${q}`);
+  
+const msg = `
+            *TIKTOK DOWNLOADER* 
+
+
+• *Title* - ${tiktok.result.title}
+
+• *Author* - ${tiktok.result.author}
+
+• *Duration* - ${tiktok.result.duration}
+
+• *Views* - ${tiktok.result.views}   
+`
+  
+// SEND DETAILS
+await conn.sendMessage( from, { image: { url: tiktok.result.cover || '' }, caption: msg }, { quoted: mek });
+
+// SEND WATER MARK VIDEO
+await conn.sendMessage(from, { video: { url: tiktok.result.wmVideo }, mimetype: "video/mp4", caption: `${tiktok.result.title}\n\nWATERMARK VIDEO ✅` }, { quoted: mek });
+  
+// SEND HD VIDEO
+await conn.sendMessage(from, { video: { url: tiktok.result.hdVideo }, mimetype: "video/mp4", caption: `${tiktok.result.title}\n\nNO-WATERMARK VIDEO ✅` }, { quoted: mek });
+  
+// SEND AUDIO
+await conn.sendMessage(from, { audio: { url: tiktok.result.sound }, mimetype: "audio/mpeg" }, { quoted: mek });
+
+  
+} catch (e) {
+console.log(e)
+reply(e)
+}
+})
+
+// 
