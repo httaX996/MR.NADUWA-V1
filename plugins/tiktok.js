938694dfdec5f5a925f3607b8421c78cebfaf6c2
𝗛.𝗔.𝗡𝗔𝗗𝗨𝗡 𝗦𝗔𝗗𝗜𝗦𝗛𝗞𝗔 <vnaduwa@gmail.com>
2024-12-28 23:46:28 +0530

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -1,58 +1,36 @@
-// TIKTOK DOWNLOAD COMMAND
+const axios = require("axios");
+const cheerio = require("cheerio");
 
-const { cmd } = require('../command')
-const { fetchJson } = require('../lib/functions')
+const clean = (data) => {
+  let regex = /(<([^>]+)>)/gi;
+  data = data.replace(/(<br?\s?\/>)/gi, " \n");
+  return data.replace(regex, "");
+};
 
-const apilink = 'https://pikabotzapi.vercel.app/?apikey=anya-md' // API LINK ( DO NOT CHANGE THIS!! )
-
-
-cmd({
-    pattern: "tiktok",
-    alias: ["tt","ttdown"],
-    react: "",
-    desc: "",
-    category: "download",
-    use: '.tiktok < url >',
-    filename: __filename
-},
-async(conn, mek, m,{from, quoted, reply, q }) => {
-try{
-  
-if(!q) return await reply("Please give me tiktok url");
-  if(!q.includes('tiktok.com')) return await reply("This url is invalid");
-  
-const tiktok = await fetchJson(`${apilink}/download/tiktok?url=${q}`);
-  
-const msg = `
-            *TIKTOK DOWNLOADER* 
-
-
-• *Title* - ${tiktok.result.title}
-
-• *Author* - ${tiktok.result.author}
-
-• *Duration* - ${tiktok.result.duration}
-
-• *Views* - ${tiktok.result.views}   
-`
-  
-// SEND DETAILS
-await conn.sendMessage( from, { image: { url: tiktok.result.cover || '' }, caption: msg }, { quoted: mek });
-
-// SEND WATER MARK VIDEO
-await conn.sendMessage(from, { video: { url: tiktok.result.wmVideo }, mimetype: "video/mp4", caption: `${tiktok.result.title}\n\nWATERMARK VIDEO ✅` }, { quoted: mek });
-  
-// SEND HD VIDEO
-await conn.sendMessage(from, { video: { url: tiktok.result.hdVideo }, mimetype: "video/mp4", caption: `${tiktok.result.title}\n\nNO-WATERMARK VIDEO ✅` }, { quoted: mek });
-  
-// SEND AUDIO
-await conn.sendMessage(from, { audio: { url: tiktok.result.sound }, mimetype: "audio/mpeg" }, { quoted: mek });
-
-  
-} catch (e) {
-console.log(e)
-reply(e)
+async function shortener(url) {
+  return url;
 }
-})
 
-// 
+exports.Tiktok = async(query) => {
+  let response = await axios("https://lovetik.com/api/ajax/search", {
+    method: "POST",
+    data: new URLSearchParams(Object.entries({ query })),
+  });
+
+  result = {};
+
+  result.creator = "YNTKTS";
+  result.title = clean(response.data.desc);
+  result.author = clean(response.data.author);
+  result.nowm = await shortener(
+    (response.data.links[0].a || "").replace("https", "http")
+  );
+  result.watermark = await shortener(
+    (response.data.links[1].a || "").replace("https", "http")
+  );
+  result.audio = await shortener(
+    (response.data.links[2].a || "").replace("https", "http")
+  );
+  result.thumbnail = await shortener(response.data.cover);
+  return result;
+    }
