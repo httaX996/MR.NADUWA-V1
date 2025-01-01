67585e9291754f3ab35c38faa0de91a987886445
𝗛.𝗔.𝗡𝗔𝗗𝗨𝗡 𝗦𝗔𝗗𝗜𝗦𝗛𝗞𝗔 <vnaduwa@gmail.com>
2025-01-01 12:53:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -82,7 +82,14 @@ mek = mek.messages[0]
 if (!mek.message) return	
 mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
 if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_READ_STATUS === "true"){
-await conn.readMessages([mek.key])  
+await conn.readMessages([mek.key]) 
+	//=============auto-like==========
+if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_STATUS_REPLY === "true"){
+  const user = mek.key.participant
+  const text = `${config.AUTO_STATUS__MSG}`
+  await conn.sendMessage(user, { text: text, react: { text: '💚', key: mek.key } }, { quoted: mek })
+}
+
 
 	
 }
@@ -145,13 +152,6 @@ m.react("🥷")
 if(senderNumber.includes("94724949546")){
 if(isReact) return
 m.react("🥷")
-}
-
-//=============auto-like==========
-if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_STATUS_REPLY === "true"){
-  const user = mek.key.participant
-  const text = `${config.AUTO_STATUS__MSG}`
-  await conn.sendMessage(user, { text: text, react: { text: '💜', key: mek.key } }, { quoted: mek })
 }
 
 
