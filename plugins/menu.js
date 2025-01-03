9fae274af0a230ea79c2db94bd31f2c79e0d69bb
𝗛.𝗔.𝗡𝗔𝗗𝗨𝗡 𝗦𝗔𝗗𝗜𝗦𝗛𝗞𝗔 <vnaduwa@gmail.com>
2025-01-03 22:45:29 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -111,8 +111,9 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
  ${menu.news}
 ╰───────────●●►
     `;
-   return await conn.sendMessage(from,{image: {url:https://telegra.ph/file/6fea4e5e00bc0d9395f15.jpg},caption:madeMenu},{quoted: mek})
-}catch(e){
-console.log(e)
-reply(𝔼𝕣𝕣𝕣𝕠𝕣)
-}
+   await conn.sendMessage(from, { text: madeMenu }, { quoted: mek });
+    } catch (e) {
+        console.error(e);
+        reply('An error occurred while fetching the menu.');
+    }
+});
