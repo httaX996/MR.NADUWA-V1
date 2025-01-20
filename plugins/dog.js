f555504e68e2567053ad4d36f35fdc5548011f13
𝗛.𝗔.𝗡𝗔𝗗𝗨𝗡 𝗦𝗔𝗗𝗜𝗦𝗛𝗞𝗔 <vnaduwa@gmail.com>
2025-01-20 16:52:44 +0530

    MR.NADUWA

--- plugins/dog.js
+++ plugins/dog.js
@@ -14,7 +14,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const response = await axios.get(apiUrl);
         const data = response.data;
 
-        await conn.sendMessage(from, { image: { url: data.message }, caption: 'MR.NADUWA-V1 DOWNLOAD📂DOG PICS\n\n\n> *BY MR DINESH*' }, { quoted: mek });
+        await conn.sendMessage(from, { image: { url: data.message }, caption: 'MR.NADUWA-V1 DOWNLOAD📂DOG PICS\n\n\n> *BY MR NADUWA*' }, { quoted: mek });
     } catch (e) {
         console.log(e);
         reply(`Error Fetching Dog Image🤕: ${e.message}`);
