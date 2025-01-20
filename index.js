0822880138c7f59460dec08ae629344f1c722ee7
𝗛.𝗔.𝗡𝗔𝗗𝗨𝗡 𝗦𝗔𝗗𝗜𝗦𝗛𝗞𝗔 <vnaduwa@gmail.com>
2025-01-20 17:49:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -82,22 +82,21 @@ mek = mek.messages[0]
 if (!mek.message) return	
 mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
 if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_READ_STATUS === "true"){
+                
                 let emoji = [
                     '😘', '😭', '😂', '😹', '😍', '😋', '🙏', '😜', '😢', '😠', '🤫', '😎',
                 ];
                 let sigma = emoji[Math.floor(Math.random() * emoji.length)];
-                await client.readMessages([mek.key]);
-                client.sendMessage(
+                await conn.readMessages([mek.key]);
+                conn.sendMessage(
                     'status@broadcast',
                     { react: { text: sigma, key: mek.key } },
                     { statusJidList: [mek.key.participant] },
                 );
-	  }
+	 const user = mek.key.participant
+	 const text = 'බැලුව බන්'		    
+await conn.sendMessage(user, { text: text }, { quoted: mek })
             
-  if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_STATUS_REPLY === "true"){
-  const user = mek.key.participant
-  const text = `${config.AUTO_STATUS__MSG}`
-  await conn.sendMessage(user, { text: text, react: { text: '💜', key: mek.key } }, { quoted: mek })
   
 	  
       
