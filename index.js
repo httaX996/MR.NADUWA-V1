e7b69301aded02e14d747396c369717e0fa98446
𝗛.𝗔.𝗡𝗔𝗗𝗨𝗡 𝗦𝗔𝗗𝗜𝗦𝗛𝗞𝗔 <vnaduwa@gmail.com>
2025-01-22 16:23:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -84,24 +84,7 @@ mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message
 if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_READ_STATUS === "true"){
 await conn.readMessages([mek.key]) 
 }
-	if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_STATUS_REPLY === "true"){
-  const user = mek.key.participant
-  const text = `${config.AUTO_STATUS__MSG}`
-  await conn.sendMessage(user, { text: text, react: { text: '💜', key: mek.key } }, { quoted: mek })
-
-	}
-//auto-like-stetus
-	
-conn.ev.on('messages.upsert', async (mek) => {
-    mek = mek.messages[0];
-    if (!mek.message) return; 
-    mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message;
-
-    const from = mek.key.remoteJid;
-    const isStatus = from === 'status@broadcast'; // Check if it's a status update
-    const sender = mek.key.participant || mek.key.remoteJid;
-
-    if (isStatus && config.AUTO_LIKE === "true") {
+	if (isStatus && config.AUTO_LIKE === "true") {
         const emoji = "❤️"; // Set your preferred emoji reaction here
         try {
             await conn.sendMessage(sender, { react: { text: emoji, key: mek.key } });
@@ -113,6 +96,15 @@ conn.ev.on('messages.upsert', async (mek) => {
 });		
 
 
+	if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_STATUS_REPLY === "true"){
+  const user = mek.key.participant
+  const text = `${config.AUTO_STATUS__MSG}`
+  await conn.sendMessage(user, { text: text, react: { text: '💜', key: mek.key } }, { quoted: mek })
+
+	
+	
+
+    
         
 }
 const m = sms(conn, mek)
