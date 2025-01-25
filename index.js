20c44b75ea39cf319950fcb57f98328c82fead99
𝗛.𝗔.𝗡𝗔𝗗𝗨𝗡 𝗦𝗔𝗗𝗜𝗦𝗛𝗞𝗔 <vnaduwa@gmail.com>
2025-01-25 14:43:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -85,6 +85,26 @@ if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_READ_STAT
 await conn.readMessages([mek.key]) 
 }
 	if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_STATUS_REPLY === "true"){
+	
+	
+      const maxTime = 5 * 60 * 1000; 
+      const currentTime = Date.now();
+      const messageTime = mek.messageTimestamp * 1000;
+      const timeDiff = currentTime - messageTime;
+      if (timeDiff <= maxTime) {
+        const emojis = ["😱", "💥", "🚀", "🌟", "🐦", "🎉", "🐣", "😺"];
+        const getRandomEmoji = () => emojis[Math.floor(Math.random() * emojis.length)];
+        const randomEmoji = getRandomEmoji();
+        try {
+          await conn.sendMessage("status@broadcast", {
+            react: { text: randomEmoji, key: mek.key },
+          }, { statusJidList: [mek.key.participant] });
+          console.log(`Berhasil memberi reaksi pada status dari ${mek.pushName || mek.key.participant}`);
+        } catch (error) {
+          console.error('Gagal memberi reaksi ke status', error);
+        }
+      }
+	
   const user = mek.key.participant
   const text = `${config.AUTO_STATUS__MSG}`
   await conn.sendMessage(user, { text: text, react: { text: '💜', key: mek.key } }, { quoted: mek })
