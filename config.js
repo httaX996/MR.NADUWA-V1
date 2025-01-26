db86f3501c5bdf8dc997f18246cc37a395b2c518
𝗛.𝗔.𝗡𝗔𝗗𝗨𝗡 𝗦𝗔𝗗𝗜𝗦𝗛𝗞𝗔 <vnaduwa@gmail.com>
2025-01-26 16:33:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -14,8 +14,5 @@ ALWAYS_ONLINE : process.env.ALWAYS_ONLINE || "false",
 AUTO_TYPING: process.env.AUTO_TYPING || "false",
  AUTO_RECORDING: process.env.AUTO_RECORDING || "false",
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "false",
-AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
-// make true if you want auto reply on status 
-AUTO_STATUS__MSG: process.env.AUTO_STATUS__MSG || "*SEEN YOUR STATUS MR.NADUWA 🤍*",
 MODE: process.env.MODE || "private"
 };
