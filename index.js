9457e25895264511a77168fba74d7d8e18982b44
TDD-GANGS <Technicaldarkdevils@gmail.com>
2025-02-01 17:52:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ console.log("Session downloaded ✅")
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 8000;
-
+const path = require('path')
 //=============================================
 
 async function connectToWA() {
